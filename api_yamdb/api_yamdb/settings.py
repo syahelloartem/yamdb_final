5d4675bdf0805fc865c67ac930841fb9ab4efc02
Артём Кувшинов <sayhelloartem@gmail.com>
2023-04-29 21:11:58 +0300

    fix

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'default_key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["158.160.96.255"]
 
 
 # Application definition
